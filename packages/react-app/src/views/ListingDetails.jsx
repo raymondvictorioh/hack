7aaa43fde840f3a86f33c81675cbf1fc5ca448a2
Raymond Victorio Handoko <raymondhandoko@Raymonds-MacBook-Pro.local>
2022-02-05 22:25:49 +0800

    add image thingy

--- packages/react-app/src/views/ListingDetails.jsx
+++ packages/react-app/src/views/ListingDetails.jsx
@@ -1,35 +1,28 @@
-import React, {useEffect, useState} from "react";
-import {
-    BrowserRouter as Router,
-    useParams
-  } from "react-router-dom";
-import { Card, Avatar, Button } from "antd";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, useParams } from "react-router-dom";
+import { Card, Image, Button } from "antd";
 import { referralList } from "./tempData/referralList";
 
 const { Meta } = Card;
 
-
 function ListingDetails() {
-    let { id } = useParams();
-    const itemDetail = () => {
-        return referralList.filter(obj => obj.id == id)[0]
-    }
-    
-    const [size, setSize] = "large"
+  let { id } = useParams();
+  const itemDetail = () => {
+    return referralList.filter(obj => obj.id == id)[0];
+  };
+
+  const [size, setSize] = "large";
   return (
     <div>
-        <h1>   
-            {itemDetail().title}
-        </h1>
-        <h1>   
-            {itemDetail().desc}
-        </h1>
-       
-        <Button type="primary" size={size}>
-          Promote
-        </Button>
+      <Image width={200} src={itemDetail().imageUrl} />
+      <h1>{itemDetail().title}</h1>
+      <h1>{itemDetail().desc}</h1>
+
+      <Button type="primary" size={size}>
+        Promote
+      </Button>
     </div>
   );
 }
 
-export default ListingDetails
+export default ListingDetails;
