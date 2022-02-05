8b3cd53ee9590f11e8b5928d0111729156d28262
Raymond Victorio Handoko <raymondhandoko@Raymonds-MacBook-Pro.local>
2022-02-06 00:17:36 +0800

    Buying Link UI template

--- packages/react-app/src/views/index.js
+++ packages/react-app/src/views/index.js
@@ -4,3 +4,5 @@ export { default as Hints } from "./Hints";
 export { default as Subgraph } from "./Subgraph";
 export { default as ListingCard } from "./ListingCard";
 export { default as ListingDetails } from "./ListingDetails";
+export { default as BuyingDetails } from "./BuyingDetails";
+export { default as MenuBar } from "./MenuBar"
