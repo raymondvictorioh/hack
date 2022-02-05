6d9f7f02aeae1dce6052aec00b22fb0ee2261ff8
Raymond Victorio Handoko <raymondhandoko@Raymonds-MacBook-Pro.local>
2022-02-05 21:24:04 +0800

    Barebones UI for viewing all NFT projects & referral details

--- packages/react-app/src/components/Header.jsx
+++ packages/react-app/src/components/Header.jsx
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <a href="https://github.com/austintgriffith/scaffold-eth" target="_blank" rel="noopener noreferrer">
       <PageHeader
-        title="🏗 scaffold-eth"
-        subTitle="forkable Ethereum dev stack focused on fast product iteration"
+        title="🖼️ REFER-MY-NFT"
+        subTitle="Sign up. Promote. Get Rewarded."
         style={{ cursor: "pointer" }}
       />
     </a>
