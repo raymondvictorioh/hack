a0aad1595160a92772c4ee8d83f82dd76cc318cb
aloysius <alloystory@gmail.com>
2022-02-06 12:29:44 +0800

    create base implementation for a few functions

--- packages/hardhat/contracts/WagmiContract.sol
+++ packages/hardhat/contracts/WagmiContract.sol
@@ -2,34 +2,37 @@ pragma solidity >=0.8.0;
 //SPDX-License-Identifier: MIT
 
 import "hardhat/console.sol";
-
-// import "@openzeppelin/contracts/access/Ownable.sol";
-// https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 
 contract WagmiContract {
+  using SafeMath for uint256;
+
   struct Listing {
+    // a boolean to check that the listing exists.
+    bool exists;
     // address of the NFT contract.
     address tokenAddr;
     // id of the token on the NFT contract.
     uint256 tokenId;
     // address of the owner.
     address ownerAddr;
-    // price of the NFT to be sold at.
+    // price of the NFT to be sold at, in base 10^18.
     uint256 listPrice;
-    // commission given to promoters for referring people.
+    // commission given to promoters for referring people, in base 10^2.
     uint256 promoterReward;
-    // discount given to buyers if they are referred.
+    // discount given to buyers if they are referred, in base 10^2.
     uint256 buyerReward;
   }
 
   // the NFTs that are being sold on Wagmi.
-  Listing[] listings;
+  uint256 listingId = 0;
+  mapping(uint256 => Listing) listings;
 
   // the set of registered promoters. mapping used for O(1) access.
   mapping(address => bool) promoters;
 
-  event NewListing(uint256 listingId);
-  event BoughtListing(uint256 listingId);
+  event NewListing(uint256 _listingId);
+  event BoughtListing(uint256 _listingId);
 
   /**
    * Gives WagmiContract the authority to manage the owner's NFT.
@@ -40,47 +43,73 @@ contract WagmiContract {
 
   /**
    * Lists an NFT on the Wagmi marketplace.
-   * @param tokenAddr address of the NFT contract.
-   * @param tokenId id of the token on the NFT contract.
-   * @param listPrice price of the NFT to be sold at.
-   * @param promoterReward commission given to promoters for referring people.
-   * @param buyerReward discount given to buyers if they are referred.
+   * @param _tokenAddr address of the NFT contract.
+   * @param _tokenId id of the token on the NFT contract.
+   * @param _listPrice price of the NFT to be sold at.
+   * @param _promoterReward commission given to promoters for referring people.
+   * @param _buyerReward discount given to buyers if they are referred.
+   * todo: ensure that msg.sender is owner of NFT.
    */
   function listNFT(
-    address tokenAddr,
-    uint256 tokenId,
-    uint256 listPrice,
-    uint256 promoterReward,
-    uint256 buyerReward
-  ) external payable {}
+    address _tokenAddr,
+    uint256 _tokenId,
+    uint256 _listPrice,
+    uint256 _promoterReward,
+    uint256 _buyerReward
+  ) external payable returns (uint256) {
+    uint256 expectedDeposit = _listPrice.mul(
+      _promoterReward.add(_buyerReward).div(100)
+    );
+    require(msg.value == expectedDeposit, "Expected deposit is wrong");
+
+    listings[listingId] = Listing({
+      exists: true,
+      tokenAddr: _tokenAddr,
+      tokenId: _tokenId,
+      ownerAddr: msg.sender,
+      listPrice: _listPrice,
+      promoterReward: _promoterReward,
+      buyerReward: _buyerReward
+    });
+    emit NewListing(listingId);
+    return listingId++;
+  }
 
   /**
    * Retrieves the information of a listing on the Wagmi marketplace.
-   * @param listingId id of the listing.
+   * @param _listingId id of the listing.
    * @return the information of the listing.
    */
-  function getListing(uint256 listingId)
+  function getListing(uint256 _listingId)
     external
     view
     returns (Listing memory)
-  {}
+  {
+    require(listings[_listingId].exists == true, "listing does not exist");
+    return listings[_listingId];
+  }
 
   /**
    * Delist an NFT from the Wagmi marketplace.
-   * @param listingId id of the listing.
+   * @param _listingId id of the listing.
    */
-  function removeListing(uint256 listingId) external {}
+  function removeListing(uint256 _listingId) external {
+    require(listings[_listingId].exists == true, "listing does not exist");
+    delete listings[_listingId];
+  }
 
   /**
    * Allow people to sign up as promoters on the Wagmi marketplace.
    */
-  function promoterSignUp() external {}
+  function promoterSignUp() external {
+    promoters[msg.sender] = true;
+  }
 
   /**
    * Purchase an NFT.
-   * @param listingId id of the listing.
-   * @param promoterAddr address of the referrer. the null address (0x0)
+   * @param _listingId id of the listing.
+   * @param _promoterAddr address of the referrer. the null address (0x0)
    * is used if the NFT is bought without a promoter address.
    */
-  function buyNFT(uint256 listingId, address promoterAddr) external payable {}
+  function buyNFT(uint256 _listingId, address _promoterAddr) external payable {}
 }
